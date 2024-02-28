55d21c4026400614d3cdcc70ee9ca1b8911251c5
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-02-28 10:54:54 +0100

    BoxShipment

---
+++ src/main/java/com/example/BoxShipment.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class BoxShipment extends BoxWeight {
+    public double cost;
+
+    public BoxShipment(double x, double y, double z, double weight, double cost) {
+        super(x, y, z, weight);
+        this.cost = cost;
+    }
+
+    public BoxShipment(Box box, double weight, double cost) {
+        super(box.x, box.y, box.z, weight);
+        this.cost = cost;
+    }
+
+}
